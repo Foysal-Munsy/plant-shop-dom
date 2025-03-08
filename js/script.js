66ab0fe67e5698d28b6d049fce2e8e2f5aed3491
Foysal-Munsy <bdfoysal555@gmail.com>
2025-03-08 22:26:48 +0600

    products show

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// document
+//   .getElementById("product-container")
+//   .addEventListener("click", function (e) {
+//     if (e.target.tagName == "BUTTON") {
+//       const button = e.target;
+//       // console.log(
+//       //   button.parentNode.parentNode.children[1].children[1].innerText
+//       // );
+//     }
+//   });
+// console.log("are you ok?");
+document
+  .getElementById("product-container")
+  .addEventListener("click", function (event) {
+    console.log(event.target.tag);
+  });
